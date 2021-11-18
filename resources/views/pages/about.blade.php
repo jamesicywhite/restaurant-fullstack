f00f0817a905bed2c00b7603089a77bce5a625bd
James White <jameswhite@Jamess-MacBook-Air.local>
2021-11-17 17:33:14 -0700

    Made layouts responsive for Mobile and Tablets

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,7 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="welcome-jumbo">
-        <h1>About Page</h1>
+    <div id="about-page">
+        <div class="content-box">
+            <div class="row">
+                <div class="col-md-6">
+                    <h1>About Page</h1>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet ullam commodi reiciendis molestias quo nostrum 
+                        in facere? Sapiente quasi eos similique hic quaerat sunt magni tempora sequi ad amet, quos distinctio ratione 
+                    </p>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet ullam commodi reiciendis molestias quo nostrum 
+                        in facere? Sapiente quasi eos similique hic quaerat sunt magni tempora sequi ad amet, quos distinctio ratione  
+                    </p>
+                </div>
+                <div class="col-md-6">
+                    <img src="/img/about-page-img.jpeg" alt="" class="img-fluid">
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
